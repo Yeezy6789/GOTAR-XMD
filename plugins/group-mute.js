d3eb4b7c71551e2165d112397d7336996f759022
DybyTech <dybytech00@gmail.com>
2025-05-23 01:39:07 -0400

    Update group-mute.js

--- plugins/group-mute.js
+++ plugins/group-mute.js
@@ -4,7 +4,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 
 cmd({
     pattern: "mute",
-    alias: ["groupmute"],
+    alias: ["groupmute", "group-close"],
     react: "🔇",
     desc: "Mute the group (Only admins can send messages).",
     category: "group",
